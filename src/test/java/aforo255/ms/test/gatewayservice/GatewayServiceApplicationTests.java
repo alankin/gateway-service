10699379e4227a7239ade7a4fb745eb7cfba5eb2
Alain Quinones <galanq@gmail.com>
2020-08-31 23:50:13 -0400

    initial commit

---
+++ src/test/java/aforo255/ms/test/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aforo255.ms.test.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
